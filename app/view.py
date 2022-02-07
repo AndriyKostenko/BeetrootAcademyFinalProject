f2922cb9c144e21199a81d9059a1478534d3029b
Andriy Kostenko <a.kostenkouk@gmail.com>
2022-02-07 11:25:33 +0200

    new update

--- app/view.py
+++ app/view.py
@@ -522,9 +522,9 @@ def main():
     personal_data_conv_handler = ConversationHandler(
         entry_points=[CommandHandler('add_or_update_info', add_info)],
         states={
-            NAME: [MessageHandler(Filters.text & (~Filters.command), name_handler, pass_user_data=True)],
-            SURNAME: [MessageHandler(Filters.text & (~Filters.command), surname_handler, pass_user_data=True)],
-            AGE: [MessageHandler(Filters.text & (~Filters.command), age_handler, pass_user_data=True)],
+            NAME: [MessageHandler(Filters.text & Filters.command, name_handler, pass_user_data=True)],
+            SURNAME: [MessageHandler(Filters.text & Filters.command, surname_handler, pass_user_data=True)],
+            AGE: [MessageHandler(Filters.text & Filters.command, age_handler, pass_user_data=True)],
             HEIGHT: [MessageHandler(Filters.text & (~Filters.command), height_handler, pass_user_data=True)],
             WEIGHT: [MessageHandler(Filters.text & (~Filters.command), weight_handler, pass_user_data=True)],
             PULSE: [MessageHandler(Filters.text & (~Filters.command), pulse_handler, pass_user_data=True)],
