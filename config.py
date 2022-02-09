a480752173a4104bb2872988536d4fa2be114cdd
Andriy Kostenko <91188777+AndriyKostenko@users.noreply.github.com>
2022-02-09 13:08:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'users.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TBOT_TOKEN = '5075706698:AAGFVZzWp0hZ4IEuyg0SnI71hy2NDXDuQPA'
-    APP_URL = 'https://telegramsportbot.herokuapp.com/'
+    TBOT_TOKEN = '************'
+    APP_URL = '***************'
 
 
