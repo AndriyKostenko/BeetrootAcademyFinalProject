6adee7c46e0b0d814c6369a9c217caea29131262
Andriy Kostenko <a.kostenkouk@gmail.com>
2022-02-09 11:43:22 +0200

    adding files

--- app/view.py
+++ app/view.py
@@ -10,6 +10,7 @@ from app import db, fapp
 from config import Config
 from fpdf import FPDF
 
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
